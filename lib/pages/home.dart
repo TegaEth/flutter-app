0543ae1ddf47cd95f72422b367b545d38fb8a081
TegaEth <ochiojie@gmail.com>
2025-04-04 14:21:05 +0300

    init

---
+++ lib/pages/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
